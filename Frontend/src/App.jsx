a621121f7abaf4ef902e775da9e0f532374f8da9
Prabesh Acharya <praveshach1032@gmail.com>
2026-02-28 10:59:59 +0545

    feat: implement routing in App component and enhance Hero section layout

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,20 @@
 
 import './App.css'
 import MasterLayout from './layouts/MasterLayouts'
+import Home from './pages/home/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-    <MasterLayout />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MasterLayout />} >
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
